6365b8790463498ca80a5f5152dc60d8de30ed6e
Alerrando Breno <alerrando2@gmail.com>
2022-12-25 18:52:04 -0300

    add function addCircle

--- 01 - Challenge/src/App.tsx
+++ 01 - Challenge/src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, MouseEvent } from 'react'
 import './App.css'
 
 export function App() {
 
   return (
-    <div className="">
+    <div className="container" onClick={ e => addCircle(e) }>
       <h1>Hello World</h1>
     </div>
   )
+
+  function addCircle(e: MouseEvent){
+    console.log(e.clientX, e.clientY);
+  }
 }
