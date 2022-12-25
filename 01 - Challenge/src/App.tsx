9c851e6ce886dac2fb6a190488376ba491146794
Alerrando Breno <alerrando2@gmail.com>
2022-12-25 20:53:55 -0300

    add funcionality to addCircle function

--- 01 - Challenge/src/App.tsx
+++ 01 - Challenge/src/App.tsx
@@ -1,15 +1,33 @@
 import { useState, MouseEvent } from 'react'
 import './App.css'
 
+type AppProps = {
+  top: number;
+  left: number;
+}
+
 export function App() {
+  const [listCircle, setListCircle] = useState<any[]>([]);
+  const [listRecover, setListRecover] = useState<any[]>([]);
 
   return (
     <div className="container" onClick={ e => addCircle(e) }>
-      <h1>Hello World</h1>
+      {listCircle.map((circle: AppProps) => {
+        return(
+          <>
+            <span className='circle' style={{left: `${circle.left}px`, top: `${circle.top}px`}}></span>
+          </>
+        )
+      })}
     </div>
   )
 
   function addCircle(e: MouseEvent){
-    console.log(e.clientX, e.clientY);
+    const coordinates: AppProps = {
+      top: e.clientY,
+      left: e.clientX
+    } 
+    
+    setListCircle([...listCircle ,coordinates]);
   }
 }
