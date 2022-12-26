6341303d66974d93fcec2e1ed2c59b4386588482
Alerrando Breno <alerrando2@gmail.com>
2022-12-26 20:00:14 -0300

    challenge complete

--- 02 - Challenge/src/App.tsx
+++ 02 - Challenge/src/App.tsx
@@ -1,10 +1,7 @@
-import './app.css';
-import React from 'react';
-import { login } from './utils/index.js'
-import { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-
-
+import "./app.css";
+import React from "react";
+import { login } from "./utils/index.js";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 // Instruções:
 // * Você tem um formulário de login INCOMPLETO
@@ -18,32 +15,57 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
 export function LoginForm() {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { isValid, errors, isDirty },
+  } = useForm<FormInputs>();
 
   return (
-    <div className='wrapper'>
-      <form className='login-form' onSubmit={handleSubmit(submit)}>
+    <div className="wrapper">
+      <form className="login-form" onSubmit={handleSubmit(submit)}>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
-        <div className='row'>
-          <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' {...register("email", {required: true, minLength: 0})} />
-        </div>
-        <div className='row'>
-          <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} {...register("password", {required: true, minLength: 6})} />
+        <div className="row">
+          <label htmlFor={"email"}>Email:</label>
+          <input
+            id={"email"}
+            type={"email"}
+            autoComplete="off"
+            {...register("email", { required: true, minLength: 0 })}
+          />
         </div>
 
-        <div className='button'>
-          <button disabled={!formState.isValid}>Login</button>
+        <div className="row">
+          <label htmlFor={"password"}>Password:</label>
+          <input
+            id={"password"}
+            type={"password"}
+            {...register("password", { required: true, minLength: 6 })}
+          />
+        </div>
+    
+        <div className="button">
+          <button disabled={!isValid}>Login</button>
         </div>
       </form>
     </div>
   );
 
   function submit(e) {
-    console.log(e)
+    login(e.email, e.password)
+      .then(() => {
+        alert("login success");
+      })
+      .catch(() => {
+        alert("login failed");
+      })
   }
 }
--- 02 - Challenge/src/utils/index.js
+++ 02 - Challenge/src/utils/index.js
@@ -1,9 +1,9 @@
-export function login({ email, password }) {
+export function login(email, password) {
   const delay = (0.7 + Math.random() * 2) * 1000;
 
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      if (password === "password123" && !!email) {
+      if (password == "password123" && !!email) {
         resolve();
       } else {
         reject({ message: "e-mail or password wrong." });
